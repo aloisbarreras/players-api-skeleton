3e13f800de146a5a896840277ee80b42e96e1c51
Alois Barreras <alois@vicino.io>
2017-08-08 18:37:46 -0400

    app.use express validator

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const apiRoutes = require('./api');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(bodyParser.json());
 
 // api routes
 app.use(apiRoutes);
+app.use(expressValidator());
 
 // 404 handler
 app.use((req, res, next) => {
