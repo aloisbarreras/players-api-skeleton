538dad05307e2f90f8ed74e5e1df4c1885fd02bd
Alois Barreras <alois@vicino.io>
2017-08-08 17:37:06 -0400

    added express app and some basic route error handling

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
-// TODO
+const express = require('express');
 
-module.exports = {};
+const app = express();
+
+// 404 handler
+app.use((req, res, next) => {
+  const err = new Error('Route not found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+module.exports = app;
