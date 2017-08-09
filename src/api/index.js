2798e0ef9f500f22cff6bcfb2e037c80dc3aafab
Alois Barreras <alois@vicino.io>
2017-08-08 23:13:27 -0400

    renamed file

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const auth = require('./auth-api');
 const user = require('./user-api');
-const player = require('./player-api');
+const players = require('./players-api');
 
 const router = Router();
 
 router.use(auth);
 router.use('/user', user);
-router.use('/players', player);
+router.use('/players', players);
 
 module.exports = router;
