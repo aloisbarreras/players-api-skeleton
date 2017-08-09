aa4bee932af431c1f858f629c911864381589c27
Alois Barreras <alois@vicino.io>
2017-08-09 12:37:55 -0400

    couple comments

--- src/middleware/jwt-verify.js
+++ src/middleware/jwt-verify.js
@@ -2,11 +2,14 @@ const jwt = require('../util/jwt');
 
 module.exports = function verifyJwt() {
   return (req, res, next) => {
+    // grab the header
     const bearerHeader = req.header('Authorization');
     if (typeof bearerHeader !== 'undefined') {
+      // grab the second half, it has the jwt
       const bearer = bearerHeader.split(' ');
       const bearerToken = bearer[1];
-      // add the decoded token to the request object
+
+      // decode it and add the decoded token to the request object
       jwt.verify(bearerToken)
         .then((decoded) => {
           req.token = decoded;
